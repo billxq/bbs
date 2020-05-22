f93b1260b5a71829b109181cae0a446fed70cd6a
greenfish <178919347@qq.com>
2020-05-22 10:23:35 +0800

    urls

--- post/views.py
+++ post/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def hello_django_bbs(request):
+    html = '<h1>Hello, Django BBS!</h1>'
+    return HttpResponse(html)
